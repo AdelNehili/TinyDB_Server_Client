3cd9a32e94e9f62df18c4d90614f6b7bc9eb4d0f
AdelNehili <a.nehili@hotmail.com>
2022-12-14 11:11:07 +0100

    Correction du test.sh en fonction du nouveau nom des executable .o

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@
 
 make
 
-gnome-terminal --geometry="40x24+0+0" -e "./Server"
+gnome-terminal --geometry="40x24+0+0" -e "./Server.o"
 sleep 0.5
-gnome-terminal --geometry="132x24+100+0" -e "./Client"
-gnome-terminal --geometry="132x24+100+0" -e "./Client"
+gnome-terminal --geometry="132x24+100+0" -e "./Client.o"
+gnome-terminal --geometry="132x24+100+0" -e "./Client.o"
 
 exit 
